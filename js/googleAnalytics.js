5d41280599ef4b602c9a9b5346820093ccaaac3f
Justin Sermeno <justin.sermeno@gmail.com>
2014-01-22 17:22:12 -0800

    google analytics for web

--- js/googleAnalytics.js
+++ js/googleAnalytics.js
@@ -1,8 +1,16 @@
+import device;
+
 var hasNativeEvents = NATIVE && NATIVE.plugins && NATIVE.plugins.sendEvent;
 
 var GoogleAnalytics = Class(function () {
 	this.init = function () {
 		this._globalProperties = {};
+
+		if (device.isSafari && !device.isSimulator && !window.weebyGoogleAnalyticsLoaded) {
+			console.log("*** *** *** LOAD googleAnalytics");
+			window.weebyGoogleAnalyticsLoaded = true;
+			this._loadTrackingForWeb();
+		}
 	}
 
 	this.track = function (name, data) {
@@ -23,6 +31,28 @@ var GoogleAnalytics = Class(function () {
 
 	this.setGlobalProperty = function (key, value) {
 		this._globalProperties[key] = value;
+	}
+
+	this._loadTrackingForWeb = function() {
+		try {
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+			ga('create', 'UA-47369514-1', 'wee.by');
+			ga('send', 'pageview');
+		} catch (err) {
+			console.log("*** *** ***" + err.message);
+		}
+	}
+
+	this.trackPage = function(page, displayTitle) {
+		ga('send', {
+			'hitType': 'pageview',
+			'page': '/' + page,
+			'title': displayTitle
+		});
 	}
 });
 
